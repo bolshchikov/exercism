aecd0d377dc14eebfbf1d5455d08ca018eab405e
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-01-03 19:40:50 +0200

    listops: make Length and Append work

---
+++ go/list-ops/listops.go
@@ -0,0 +1,61 @@
+package listops
+
+// IntList is a list of intergers
+type IntList []int
+
+type binFunc func(x, y int) int
+type predFunc func(n int) bool
+type unaryFunc func(x int) int
+
+// Length returns the amount of members in the list
+func (list IntList) Length() (length int) {
+	for range list {
+		length++
+	}
+	return
+}
+
+// Filter returns updates list with applied function to each member
+func (list IntList) Filter(fn predFunc) IntList {
+	return list
+}
+
+// Foldl is folding array left
+func (list IntList) Foldl(fn binFunc, arg int) int {
+	return 0
+}
+
+// Foldr is folding array right
+func (list IntList) Foldr(fn binFunc, arg int) int {
+	return 0
+}
+
+// Map applies the given function to each arr member and returns updated array
+func (list IntList) Map(fn unaryFunc) IntList {
+	return list
+}
+
+// Reverse changes the order of arr members
+func (list IntList) Reverse() IntList {
+	return list
+}
+
+// Append adds items to list
+func (list IntList) Append(appendee IntList) IntList {
+	var res = make([]int, list.Length()+appendee.Length())
+	index := 0
+	for _, v := range list {
+		res[index] = v
+		index++
+	}
+	for _, v := range appendee {
+		res[index] = v
+		index++
+	}
+	return res
+}
+
+//Concat joins two or more arrays
+func (list IntList) Concat(parts []IntList) IntList {
+	return list
+}
