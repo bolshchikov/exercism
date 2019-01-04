e36ef4002ae1530b58e832932bc177868f214623
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-01-04 19:57:02 +0200

    clock: refactor clock to store in minutes

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -2,52 +2,50 @@ package clock
 
 import "fmt"
 
-const minsInHour = 60
-const hoursInDay = 24
+const minutesInHour = 60
+const minutesInDay = 24 * 60
 
-// Clock represents time
+// Clock object
 type Clock struct {
-	hh int
-	mm int
+	mins int
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hh, c.mm)
+	hours := c.mins / minutesInHour
+	minutes := c.mins % minutesInHour
+	return fmt.Sprintf("%02d:%02d", hours, minutes)
 }
 
 // Add should add time to the given clock
 func (c Clock) Add(mins int) Clock {
-	c.hh += mins / minsInHour
-	c.mm += mins % minsInHour
+	c.mins += mins
 	return c.normalize()
 }
 
 // Subtract should subtract time to the given clock
 func (c Clock) Subtract(mins int) Clock {
-	c.hh -= mins / minsInHour
-	c.mm -= mins % minsInHour
+	c.mins -= mins
 	return c.normalize()
 }
 
 func (c Clock) normalize() Clock {
-	hoursToSubtract := 0
-	for c.mm < 0 {
-		c.mm = minsInHour + c.mm
-		hoursToSubtract++
+	for c.mins < 0 || c.mins >= minutesInDay {
+		switch {
+		case c.mins < -minutesInDay:
+			c.mins = c.mins % minutesInDay
+		case -minutesInDay < c.mins && c.mins < 0:
+			c.mins = minutesInDay + c.mins
+		case c.mins == minutesInDay:
+			c.mins = 0
+		case c.mins > minutesInDay:
+			c.mins = c.mins % minutesInDay
+		}
 	}
-	c.hh = c.hh + c.mm/minsInHour - hoursToSubtract
-	for c.hh < 0 {
-		c.hh = hoursInDay + c.hh
-	}
-	if c.hh >= hoursInDay {
-		c.hh = c.hh % hoursInDay
-	}
-	c.mm = c.mm % minsInHour
 	return c
 }
 
-// New constructs a new clock
-func New(hour, mins int) Clock {
-	c := Clock{hour, mins}
+// New creates new clock object
+func New(hh, mm int) Clock {
+	c := Clock{hh*minutesInHour + mm}
 	return c.normalize()
 }
