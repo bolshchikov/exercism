a6700e4acbb117d7712b8a9852ded4581eb15c34
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-01-06 15:04:21 +0200

    triplet: canculate triplet in range

--- go/pythagorean-triplet/pythagorean_triplet.go
+++ go/pythagorean-triplet/pythagorean_triplet.go
@@ -7,11 +7,39 @@ type Triplet struct {
 	c int
 }
 
-// Range returns list of triplets that satisfy given range
-func Range(min, max int) []Triplet {
-	return []Triplet{
-		{3, 4, 5},
+func sortTriplet(t *Triplet) *Triplet {
+	if t.a > t.b {
+		t.a, t.b = t.b, t.a
 	}
+	if t.a > t.c {
+		t.a, t.c = t.c, t.a
+	}
+	if t.b > t.c {
+		t.b, t.c = t.c, t.b
+	}
+	return t
+}
+
+// Range returns list of triplets that satisfy given range
+func Range(min, max int) (res []Triplet) {
+	t := Triplet{0, 0, 0}
+	m := 2
+	for t.c <= max {
+		for n := 1; n < m; n++ {
+			t = Triplet{
+				m*m - n*n,
+				2 * m * n,
+				m*m + n*n,
+			}
+			sortTriplet(&t)
+			if t.c > max || t.a < min {
+				continue
+			}
+			res = append(res, t)
+		}
+		m++
+	}
+	return
 }
 
 // Sum returns list of triplets that satisfy given sum
