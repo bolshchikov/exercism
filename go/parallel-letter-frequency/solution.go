6031bc7c7df11644cbb5a6a93341bbdce5a6e904
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-01-07 18:53:42 +0200

    parallel-letter: cleaner solution

--- go/parallel-letter-frequency/solution.go
+++ go/parallel-letter-frequency/solution.go
@@ -1,17 +1,5 @@
 package letter
 
-func mergeMaps(mapA, mapB FreqMap) FreqMap {
-	for k, frB := range mapB {
-		frA, ok := mapA[k]
-		if ok {
-			mapA[k] = frA + frB
-		} else {
-			mapA[k] = frB
-		}
-	}
-	return mapA
-}
-
 // ConcurrentFrequency calculates letter frequency concurrently
 func ConcurrentFrequency(input []string) FreqMap {
 	res := FreqMap{}
@@ -21,10 +9,10 @@ func ConcurrentFrequency(input []string) FreqMap {
 			c <- Frequency(t)
 		}(text)
 	}
-	received := 0
-	for received != 3 {
-		mergeMaps(res, <-c)
-		received++
+	for range input {
+		for k, v := range <-c {
+			res[k] += v
+		}
 	}
 	return res
 }
